0b105e7300518ff670fd6171a1ad6d8e35f9c2b4
テイ　キタク <teikitaku.dat@gmail.com>
2023-06-01 16:11:53 +0900

    vx,vy

--- fukuv0601/Form1.Designer.cs
+++ fukuv0601/Form1.Designer.cs
@@ -33,13 +33,15 @@
             button2 = new Button();
             label1 = new Label();
             timer1 = new System.Windows.Forms.Timer(components);
+            button3 = new Button();
+            button4 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(246, 179);
+            button1.Location = new Point(823, 381);
             button1.Name = "button1";
-            button1.Size = new Size(61, 59);
+            button1.Size = new Size(32, 32);
             button1.TabIndex = 0;
             button1.Text = "←";
             button1.UseVisualStyleBackColor = true;
@@ -47,9 +49,9 @@
             // 
             // button2
             // 
-            button2.Location = new Point(487, 179);
+            button2.Location = new Point(900, 381);
             button2.Name = "button2";
-            button2.Size = new Size(66, 59);
+            button2.Size = new Size(32, 32);
             button2.TabIndex = 1;
             button2.Text = "→";
             button2.UseVisualStyleBackColor = true;
@@ -59,11 +61,11 @@
             // 
             label1.BackColor = SystemColors.ActiveCaption;
             label1.Font = new Font("Yu Gothic UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(369, 179);
+            label1.Location = new Point(733, 183);
             label1.Name = "label1";
-            label1.Size = new Size(66, 59);
+            label1.Size = new Size(64, 64);
             label1.TabIndex = 2;
-            label1.Text = "label1";
+            label1.Text = "(0 ^ 0)";
             label1.UseCompatibleTextRendering = true;
             label1.Click += label1_Click;
             // 
@@ -72,16 +74,39 @@
             timer1.Enabled = true;
             timer1.Tick += timer1_Tick;
             // 
+            // button3
+            // 
+            button3.Location = new Point(861, 343);
+            button3.Name = "button3";
+            button3.Size = new Size(32, 32);
+            button3.TabIndex = 3;
+            button3.Text = "↑";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(861, 417);
+            button4.Name = "button4";
+            button4.Size = new Size(32, 32);
+            button4.TabIndex = 4;
+            button4.Text = "↓";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(944, 461);
+            Controls.Add(button4);
+            Controls.Add(button3);
             Controls.Add(label1);
             Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
         }
 
@@ -91,5 +116,7 @@
         private Button button2;
         private Label label1;
         private System.Windows.Forms.Timer timer1;
+        private Button button3;
+        private Button button4;
     }
 }
--- fukuv0601/Form1.cs
+++ fukuv0601/Form1.cs
@@ -1,18 +1,22 @@
+using System.Runtime.Intrinsics.X86;
+
 namespace fukuv0601
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
+        string chr = "(* - *)";
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        int vx = 10;
-
         private void label1_Click(object sender, EventArgs e)
         {
             timer1.Enabled = false;
-            label1.Text = "テイ　キタク";
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -27,7 +31,69 @@ namespace fukuv0601
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            /*
+            if (vx < 0 && label1.Left < 0)
+            {
+                label1.Left = ClientSize.Width;
+            }
+            else if (vx > 0 && label1.Right > ClientSize.Width)
+            {
+                label1.Left = 0;
+            }
+            else
+                label1.Left = label1.Left + vx;
+
+            if (vy < 0 && label1.Top < 0)
+            {
+                label1.Top = ClientSize.Height;
+            }
+            else if (vy > 0 && label1.Bottom > ClientSize.Height)
+            {
+                label1.Top = 0;
+            }
+            else
+                label1.Top = label1.Top + vy;
+            */
             label1.Left = label1.Left + vx;
+            label1.Top = label1.Top + vy;
+            if (label1.Left < 0)
+            {
+                vx = Math.Abs(vx + (vx / 10));
+
+            }
+            else if (label1.Right > ClientSize.Width)
+            {
+                
+                vx = -Math.Abs(vx + (vx / 10));
+            }    
+            if (label1.Top < 0)
+            {
+
+                vy = Math.Abs(vy + (vy / 10));
+            }    
+            else if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -Math.Abs(vy + (vy / 10));
+
+            }
+            string t = label1.Text;
+            label1.Text = chr;
+            chr = t;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            vy = -10;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            vy = 10;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
